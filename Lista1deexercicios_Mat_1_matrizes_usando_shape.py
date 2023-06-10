d90c0b0c92e14bf458d9dd1d59d92f9a7939f03c
AndrewDevos <andrew.cottadevos@gmail.com>
2023-06-10 16:22:12 -0300

    AGORA VOU TERMINAR A LISTA UM COM APENAS ESSE COD

---
+++ Lista1deexercicios_Mat_1_matrizes_usando_shape.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def Mat():
+    A = np.array([[1,2,4],[-3,0,-1],[2,1,2]])
+    B = np.array([[2,0,0],[1,-4,3],[-1,3,2]])
+
+    print('essa coluna é a coluna',A)
+    print(A)
+    print('O tamanho é:')
+    print('essa coluna é a coluna',B)
+    print(B)
